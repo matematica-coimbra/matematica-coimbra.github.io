8104405fc5285bffc0c000c2ef7799302c694973
Gonçalo Ferreira <gventuraf99@gmail.com>
2022-09-30 08:11:58 +0100

    Changing how the contact form works

--- js_code/index.js
+++ js_code/index.js
@@ -6,7 +6,26 @@ function main()
 {
     set_expand_event();
     set_close_event();
-    
+    set_form_submission();
+}
+
+function set_form_submission()
+{
+    $(".subinpu").on("click", function() {
+        let xhttp = new XMLHttpRequest();
+        let link = "https://docs.google.com/forms/d/e/1FAIpQLSd814VAVDcyLCmPhAnxnaV7zEK-C7_HfSrtyha4FulQt-tvsg/" +
+            "formResponse?usp=pp_url" +
+            "&entry.2005620554=" + $("#name").val() +
+            "&entry.1045781291=" + $("#email").val() +
+            "&entry.2024126210=" + $("#phone").val() +
+            "&entry.965650928="  + $("#comment").val()
+            ;
+            console.log(link);
+        xhttp.open("GET", encodeURI(link));
+        xhttp.send();
+        //! modify this message
+        alert("Sucesso. O explicador irá contactá-lo o mais cedo possível. Obrigado.")
+    });
 }
 
 function set_expand_event()
