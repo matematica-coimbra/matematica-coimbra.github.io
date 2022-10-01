ed41c65d15cd7f27e8c3c90ea241b9c94efce271
Gonçalo Ferreira <gventuraf99@gmail.com>
2022-10-01 17:18:03 +0100

    SEO, lang, form subsmission, design

--- js_code/index.js
+++ js_code/index.js
@@ -9,22 +9,43 @@ function main()
     set_form_submission();
 }
 
+/*
+
+https://docs.google.com/forms/d/e/1FAIpQLSd814VAVDcyLCmPhAnxnaV7zEK-C7_HfSrtyha4FulQt-tvsg/viewform?
+usp=pp_url
+&entry.2005620554=g
+&entry.1442067619=m
+&entry.2024126210=9
+&entry.965650928=c
+
+*/
+
 function set_form_submission()
 {
     $(".subinpu").on("click", function() {
+        // verify if blank
+        let name = $("#name").val();
+        let school = $("#school").val();
+        let phone = $("#phone").val();
+        if (!phone) {
+            alert("Por favor insira o nº de telemóvel ou email.");
+            return;
+        }
+        
+        // submit
         let xhttp = new XMLHttpRequest();
         let link = "https://docs.google.com/forms/d/e/1FAIpQLSd814VAVDcyLCmPhAnxnaV7zEK-C7_HfSrtyha4FulQt-tvsg/" +
             "formResponse?usp=pp_url" +
-            "&entry.2005620554=" + $("#name").val() +
-            "&entry.1045781291=" + $("#email").val() +
-            "&entry.2024126210=" + $("#phone").val() +
-            "&entry.965650928="  + $("#comment").val()
-            ;
-            console.log(link);
+            "&entry.2005620554=" + name +
+            "&entry.1442067619=" + school +
+            "&entry.2024126210=" + phone +
+            "&entry.965650928="  + $("#comment").val();
         xhttp.open("GET", encodeURI(link));
         xhttp.send();
-        //! modify this message
-        alert("Sucesso. O explicador irá contactá-lo o mais cedo possível. Obrigado.")
+        
+        // give confirmation:
+        $("#contactar").css("background-color", "#4CAF50");
+        $(".subinpu").text("Submetido com Sucesso!");
     });
 }
 
@@ -34,7 +55,7 @@ function set_expand_event()
         if ( parseFloat($(this).css("height")) > 490 )
             $(this).css({
                 "width": "800px",
-                "height": "500px"
+                "height": "550px"
             });
     });
 }
@@ -59,7 +80,7 @@ function close_contactar()
     $("#contactar").hover(function() {
         $(this).css({
             "width": "800px",
-            "height": "500px"
+            "height": "550px"
         });
     });
 }
