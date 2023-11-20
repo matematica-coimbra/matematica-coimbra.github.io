3b1608f6113fe13b423a0f8a59c39ce1e7135367
Goncalo <gventuraf99@gmail.com>
2023-11-20 17:30:30 +0000

    deploy to gh

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
 
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			strict: false,
-			pages: 'docs',
-      		assets: 'docs',
-      		fallback: null,
-		}),
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs",
+			strict: false
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        //target: "#svelte"
+    }
 };
 
 export default config;
