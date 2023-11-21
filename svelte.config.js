38105a0b6ba8e9dcfbccd3dc85018f5814877e28
Goncalo <gventuraf99@gmail.com>
2023-11-21 22:21:27 +0000

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html',
-			strict: false
+			appDir: "app",
+			strict: false,
+			pages: "docs/",
+			assets: "docs/",
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
